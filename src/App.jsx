d8f4a523873f8151018a0bcb6cf6b5b8bddee208
Anna <anutasash@mail.ru>
2024-03-06 13:54:20 +0300

    5

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,29 @@ import TodoList from './components/dz3/TodoList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/dz4/HomePage';
 import AboutPage from './components/dz4/AboutPage';
+import { ThemeContext } from './components/dz5/theme-context.jsx';
+import React from 'react';
 
 
 
 function App() {
   const { todos, addTodo, deleteTodo } = useTodoState([]);
+  const { theme, toggle, dark } = React.useContext(ThemeContext)
   return (
     <div className="App">
       <div>
-      <h1 className='title'>Домашнее задание 1</h1>
+        -----------------------------------------------------------------------------------------
+        <h1 className='title'>Домашнее задание 1</h1>
         <Message title="Homework1" />
         <Message text="Развернуть новый проект с использованием create-react-app." />
         <Message text="Создать компонент Message, отображающий переданный ему props текст." />
         <Message text="Дополнительное задание Установить расширение React Devtools." /></div>
-      <><h1 className='title'>Домашнее задание 2</h1>
+      <>
+        ------------------------------------------------------------------------------------
+        <h1 className='title'>Домашнее задание 2</h1>
         <CommentsList /></>
-        <h1 className='title'>Домашнее задание 3</h1>
+      ------------------------------------------------------------------------------
+      <h1 className='title'>Домашнее задание 3</h1>
       <TemperatureConverter />
       <>
 
@@ -37,6 +44,7 @@ function App() {
         }} />
         <TodoList todos={todos} deleteTodo={deleteTodo} />
       </>
+      ---------------------------------------------------------------------------
       <> <h1 className='title'>Домашнее задание 4</h1>
         <BrowserRouter>
           <Routes>
@@ -44,7 +52,24 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
           </Routes>
         </BrowserRouter></>
-
+      ---------------------------------------------------------------------------
+      <h1 className='title'>Домашнее задание 5</h1>
+      <header
+              style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
+        <button
+          type="button"
+          onClick={toggle}
+          style={{
+            backgroundColor: theme.backgroundColor,
+            color: theme.color,
+            outline: 'none'
+          }}
+        >
+          Toggle to {!dark ? 'Dark' : 'Light'} theme
+        </button>
+          
+      </header>
     </div>
   );
 }
